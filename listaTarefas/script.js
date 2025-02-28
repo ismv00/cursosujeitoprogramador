2c19bbcf6468a2e7413c89e991501ae2d3efb4e7
ismv00 <ismv00@icloud.com>
2025-02-27 22:20:04 -0400

    feat:projeto-lista-tarefas

---
+++ listaTarefas/script.js
@@ -0,0 +1,5 @@
+let listElement = document.querySelector("app ul");
+let inputElement = document.querySelector("app input");
+let buttonElement = document.querySelector("app button");
+
+let tarefas = [];
