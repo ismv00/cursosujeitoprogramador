4017a78507f7e94d81dfcead429b33d3fe98a2b0
ismv00 <ismv00@icloud.com>
2025-02-27 22:45:12 -0400

    feat:createElement list

--- listaTarefas/script.js
+++ listaTarefas/script.js
@@ -1,5 +1,34 @@
-let listElement = document.querySelector("app ul");
-let inputElement = document.querySelector("app input");
-let buttonElement = document.querySelector("app button");
+let listElement = document.querySelector("#app ul");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
 
 let tarefas = [];
+
+function renderTarefas() {
+  listElement.innerHTML = "";
+
+  tarefas.map((todo) => {
+    let liElement = document.createElement("li");
+    let tarefaText = document.createTextNode(todo);
+
+    liElement.appendChild(tarefaText);
+    listElement.appendChild(liElement);
+  });
+}
+
+function adcionarTarefas() {
+  if (inputElement.value === "") {
+    alert("Digite alguma tarefa!");
+    return false;
+  } else {
+    let novaTarefa = inputElement.value;
+
+    tarefas.push(novaTarefa);
+
+    inputElement.value = "";
+
+    renderTarefas();
+  }
+}
+
+buttonElement.onclick = adcionarTarefas;
