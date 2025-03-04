251ebdf147eb95ebd4e011ef06fd1d1568bdb9e1
ismv00 <ismv00@icloud.com>
2025-03-04 15:45:22 -0400

    feat:create filme detail

--- prime-flix/src/pages/Filme/index.js
+++ prime-flix/src/pages/Filme/index.js
@@ -1,5 +1,63 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./filme-info.css";
+import api from "../../services/api";
+
 function Filme() {
-  return <h1>Bem vindo a página detalhes do filme</h1>;
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "7234e0386ed6011f330886f29e665f2a",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {});
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log("Componente foi desmontado");
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes</h1>
+      </div>
+    );
+  }
+  return (
+    <div class="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} /10</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">Trailer</a>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Filme;
