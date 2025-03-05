26eb52bcc1996aff7bf374e6cdc1a64f14cf7742
ismv00 <ismv00@icloud.com>
2025-03-04 21:48:52 -0400

    feat:create filmeDelete function

--- prime-flix/src/pages/Favoritos/index.js
+++ prime-flix/src/pages/Favoritos/index.js
@@ -9,9 +9,19 @@ function Favoritos() {
     const minhaLista = localStorage.getItem("@primeflix");
     setFilmes(JSON.parse(minhaLista) || []);
   }, []);
+
+  function excluirFilme(id) {
+    let filtroFilmes = filmes.filter((item) => {
+      return item.id !== id;
+    });
+
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+  }
   return (
     <div className="meus-filmes">
       <h1>Meus Filmes</h1>
+      {filmes.length === 0 && <span>Você não tem nenhum filme salvo. =(</span>}
       <ul>
         {filmes.map((item) => {
           return (
@@ -19,7 +29,7 @@ function Favoritos() {
               <span>{item.title}</span>
               <div>
                 <Link to={`/filme/${item.id}`}>Ver Detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
               </div>
             </li>
           );
