f5f5bb09bb53c91854467d6b9a8d2e92925c9768
ismv00 <ismv00@icloud.com>
2025-03-23 14:10:58 -0400

    listatarefaReact

---
+++ ListaTarefaReact/src/App.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+export default function App() {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [input, setInput] = useState("");
+  const [editTask, setEditTasks] = useState({
+    enabled: false,
+    task: "",
+  });
+
+  function handleRegister() {
+    if (!input) {
+      alert("Preecha o nome da sua tarefa");
+      return;
+    }
+    if (editTask.enabled) {
+      handleSaveEdit();
+      return;
+    }
+
+    setTasks((tarefas) => [...tarefas, input]);
+    setInput("");
+  }
+
+  function handleSaveEdit() {
+    const findIndexTask = tasks.findIndex((task) => task === editTask.task);
+    const allTasks = [...tasks];
+    allTasks[findIndexTask] = input;
+    setTasks(allTasks);
+
+    setEditTasks({
+      enabled: false,
+      task: "",
+    });
+
+    setInput("");
+  }
+
+  function deleteRegister(item: string) {
+    const removeTask = tasks.filter((task) => task !== item);
+    setTasks(removeTask);
+  }
+
+  function handleEdit(item: string) {
+    setInput(item);
+    setEditTasks({
+      enabled: true,
+      task: item,
+    });
+  }
+  return (
+    <div>
+      <h1>Lista de Tarefas;</h1>
+
+      <input
+        placeholder="Digite o nome da tarefa..."
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+
+      <button onClick={handleRegister}>
+        {editTask.enabled ? "Atualizar Tarefa" : "Cadastrar Tarefa"}
+      </button>
+      <hr />
+      {tasks.map((item) => (
+        <section key={item}>
+          <span>{item}</span>
+          <button onClick={() => handleEdit(item)}>Editar</button>
+          <button onClick={() => deleteRegister(item)}>Excluir</button>
+        </section>
+      ))}
+    </div>
+  );
+}
